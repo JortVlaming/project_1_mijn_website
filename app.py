4b2ac6c2b675db991e0fba6e80e2ada282af5995
crystalcoding9999 <crystalcraft9999@gmail.com>
2024-09-20 20:21:43 +0200

    finished index page

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, session, redirect, url_for
+from database import Database
 
 app = Flask(__name__)
 
+app.secret_key = "7VLNMnLD3*sS2QgC"
+
+db = Database()
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+    return render_template("index.html", db=db, recents=[])
+
+@app.route('/set_token')
+def set_token():
+    session["token"] = db.generate_token("test", "test")
+    return redirect(url_for("home"))
+
+@app.route('/reset_token')
+def reset_token():
+    session["token"] = None
+
+    return redirect(url_for("home"))
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5500, debug=True)
